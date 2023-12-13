1467e8160ffc93d9cace23b3155568e4c95e901f
KANGYONGSU23 <edc20203@gmail.com>
2023-12-13 10:31:55 +0900

    setting :: 종속성 재설치

--- src/api/login/login.ts
+++ src/api/login/login.ts
@@ -1,11 +1,7 @@
 import { useQuery } from "react-query";
 import axios from "axios";
-import Store from "electron-store";
-import { userIdSchema } from "../../store/store";
-import { UserId } from "../../store/type";
 
 export function API_login(username: string) {
-  const store = new Store<UserId>({ schema: userIdSchema });
   return useQuery(
     ["login", username],
     async () => {
